1303be119cbfc533302cb40fd61e6c8061df6580
huscarter <weiwhitejx@gmail.com>
2020-06-11 11:50:01 +0800

    to init project

---
+++ app/src/main/java/com/whh/cctalk/NetConfig.kt
@@ -0,0 +1,10 @@
+package com.whh.cctalk
+
+/**
+ * Create by weihh@lianlianpay.com on 2020/6/10
+ * <p>
+ * 类说明:<BR/>
+ * 接受传值:<BR/>
+ * 对外传值:<BR/>
+ * @since
+ */
---
+++ app/src/main/java/com/whh/cctalk/PersonalFragment.kt
@@ -0,0 +1,18 @@
+package com.whh.cctalk
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * The personal page
+ */
+class PersonalFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_personal,container,false)
+    }
+
+}
